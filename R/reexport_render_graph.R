1088cc4c6a07ed1f729ae7e01a7a7481c4f0ae18
Richard Iannone <riannone@me.com>
2018-05-20 02:44:08 -0700

    Remove reexport of fcn

--- R/reexport_render_graph.R
+++
@@ -1,8 +0,0 @@
-#' Render a graph in various formats
-#' @description Using a \code{dgr_graph} object,
-#' render the graph in the RStudio Viewer.
-#' @importFrom DiagrammeR render_graph
-#' @name render_graph
-#' @rdname render_graph
-#' @export
-NULL
