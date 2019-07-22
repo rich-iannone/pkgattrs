bfbabd8464c7bef9810b3f9456717b0d0b994c45
Richard Iannone <riannone@me.com>
2019-07-22 00:45:57 -0400

    Refactor the `from_github()` fcn

--- R/from_github.R
+++ R/from_github.R
@@ -1,16 +1,16 @@
 #' Helper function for pointing to an R package on GitHub
 #'
-#' This helper function allows for inspection of R
-#' packages that are hosted on GitHub.
-#' @param repo the GitHub repository address
-#' of the following construction: \code{[username]/[repo]}.
-#' @importFrom dplyr tibble mutate
+#' This helper function allows for inspection of R packages that are hosted on
+#' GitHub.
+#' @param repo The GitHub repository address of the following construction:
+#'   \code{[username]/[repo]}.
 #' @export
 from_github <- function(repo) {
 
   # Construct the tibble with the necessary repo information
   dplyr::tibble(src = "GitHub", repo = repo) %>%
     dplyr::mutate(url = paste0(
-      "http://github.com/", repo, "/zipball/master")) %>%
+      "http://github.com/", repo, "/zipball/master")
+    ) %>%
     dplyr::mutate(pkg_path = paste0(getwd(), "/temp_pkgattrs/", repo))
 }
